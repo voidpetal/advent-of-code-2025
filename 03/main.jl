b573c027712574b652bce37e135a76ebe74d5191
Kai Hodžić <hodzic.e.k@outlook.com>
2025-12-03 14:02:06 +0100

    Optimization

--- 03/main.jl
+++ 03/main.jl
@@ -1,46 +1,33 @@
 include("../common/io_functions.jl")
 using .CommonIO
 
-get_index(bank::Array{Int}, num::Int) = findfirst(x -> x == num, bank)
-
-function find_largest_combination(bank::Array{Int}, num_batteries)
+function find_largest_combination(bank::Vector{Int}, num_batteries)
     # Base case: Not enough digits available
-    if length(bank) < num_batteries
-        return 0
-    end
+    length(bank) < num_batteries && return 0
     
     # Base case: Only need one digit, return the largest
-    if num_batteries == 1
-        largest = maximum(bank)
-        return largest
-    end
+    num_batteries == 1 && return maximum(bank)
     
     # Valid range ensures enough digits remain for subsequent selections
-    max_joltage = 0
     biggest_nums = unique(sort(bank[1:end - num_batteries + 1], rev=true))
     
     for biggest in biggest_nums
-        biggest_id = get_index(bank, biggest)
+        biggest_id = findfirst(==(biggest), bank)
         
         # Recursively find best combination for remaining positions
         next_largest = find_largest_combination(bank[biggest_id+1:end], num_batteries - 1)
-        if next_largest == 0
-            continue
-        end
-        maxx = biggest * 10^(num_batteries-1) + next_largest
         
-        return maxx
+        # If no valid combination found, skip
+        next_largest == 0 && continue
+        
+        return biggest * 10^(num_batteries-1) + next_largest
     end
-
-    return max_joltage
+    return 0
 end
 
 function solve(joltages::Vector{String}, num_batteries::Int)
-    total = 0
-    for bank in joltages
-        total += find_largest_combination(parse.(Int, split(bank, "")), num_batteries)
-    end
-    return total
+    return sum(find_largest_combination([parse(Int, c) for c in bank], num_batteries) 
+               for bank in joltages)
 end
 
 joltages = CommonIO.read_input_lines(3)
